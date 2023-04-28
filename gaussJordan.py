3324e54a14c79511267456ca80da4eaa305f6ade
Ivan Hurtado <ivan.hurtado92@outlook.com>
2023-04-28 14:53:56 -0600

    Información extra

--- gaussJordan.py
+++ gaussJordan.py
@@ -1,3 +1,21 @@
+"""
+Programación científica
+Carlos Leonardo Cruz Ortiz
+Ivan Israel Hurtado Lozano
+Gauss Jordan
+
+El método de Gauss-Jordan es un algoritmo que se usa para determinar la inversa de una matriz y
+las soluciones de un sistema de ecuaciones lineales.
+
+Un sistema de ecuaciones se resuelve por el método de Gauss cuando se obtienen sus soluciones 
+mediante la reducción del sistema dado a otro equivalente en el que cada ecuación tiene una 
+incógnita menos que la anterior. El método de Gauss transforma la matriz de coeficientes en 
+una matriz triangular superior. El método de Gauss-Jordan continúa el proceso de transformación
+hasta obtener una matriz diagonal. 
+
+Ultima actualización: 4 de abril del 2023
+"""
+
 import opcionesMatriz,os;
 
 os.system('cls')
